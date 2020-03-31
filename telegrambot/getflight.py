56a524723bade75a8e9b08b6185731bdf2683cae
mihastrah <miha.strah@student.um.si>
2020-03-31 21:59:13 +0200

    changed gitignore and added telegram bot

---
+++ telegrambot/getflight.py
@@ -0,0 +1,242 @@
+import requests, json
+import datetime
+import time
+import oauth2 as oauth
+
+def getFlightStatus(flight, date):
+    token = getToken()
+    flightstatus = getFlight(token, flight, date)
+    return flightstatus
+
+
+def getFlight(token, flight, date):
+    url = 'https://api.lufthansa.com/v1/operations/flightstatus' + '/' + flight + '/' + date
+    print(url)
+    bearer = "Bearer " + token
+    headers = {"Authorization":bearer, "Accept":"application/json"}
+    print (bearer)
+    i = 0
+    while i<10:
+        try:
+            request = requests.get(url = url, headers = headers)
+            print(request)
+            data = request.json()
+            print(data)
+            i = 10
+        except:
+            time.sleep(2)
+            print("retry " + str(i))
+            i = i + 1
+            pass
+
+    try:
+        depairport = data['FlightStatusResource']['Flights']['Flight'][0]['Departure']['AirportCode']
+    except:
+        depairport = ""
+    try:
+        depscheduled = data['FlightStatusResource']['Flights']['Flight'][0]['Departure']['ScheduledTimeLocal']['DateTime']
+    except:
+        depscheduled = ""
+    try:
+        depscheduledUTC = data['FlightStatusResource']['Flights']['Flight'][0]['Departure']['ScheduledTimeUTC']['DateTime']
+    except:
+        depscheduledUTC = ""
+    try:
+        depactual = data['FlightStatusResource']['Flights']['Flight'][0]['Departure']['ActualTimeLocal']['DateTime']
+    except:
+        depactual = ""
+    try:
+        depactualUTC = data['FlightStatusResource']['Flights']['Flight'][0]['Departure']['ActualTimeUTC']['DateTime']
+    except:
+        depactualUTC = ""
+    try:
+        depterminal = data['FlightStatusResource']['Flights']['Flight'][0]['Departure']['Terminal']['Name']
+    except:
+        depterminal = ""
+    try:
+        depgate = data['FlightStatusResource']['Flights']['Flight'][0]['Departure']['Terminal']['Gate']
+    except:
+        depgate = ""
+    try:
+        deptimestatus = data['FlightStatusResource']['Flights']['Flight'][0]['Departure']['TimeStatus']['Code']
+    except:
+        deptimestatus = ""
+    try:
+        deptimestatusdef = data['FlightStatusResource']['Flights']['Flight'][0]['Departure']['TimeStatus']['Definition']
+    except:
+        deptimestatusdef = ""
+    try:
+        arrairport = data['FlightStatusResource']['Flights']['Flight'][0]['Arrival']['AirportCode']
+    except:
+        arrairport = ""
+    try:
+        arrscheduled = data['FlightStatusResource']['Flights']['Flight'][0]['Arrival']['ScheduledTimeLocal']['DateTime']
+    except:
+        arrscheduled = ""
+    try:
+        arrscheduledUTC = data['FlightStatusResource']['Flights']['Flight'][0]['Arrival']['ScheduledTimeUTC']['DateTime']
+    except:
+        arrscheduledUTC = ""
+    try:
+        arractual = data['FlightStatusResource']['Flights']['Flight'][0]['Arrival']['ActualTimeLocal']['DateTime']
+    except:
+        arractual = ""
+    try:
+        arractualUTC = data['FlightStatusResource']['Flights']['Flight'][0]['Arrival']['ActualTimeUTC']['DateTime']
+    except:
+        arractualUTC = ""
+    try:
+        arrterminal = data['FlightStatusResource']['Flights']['Flight'][0]['Arrival']['Terminal']['Name']
+    except:
+        arrterminal = ""
+    try:
+        arrgate = data['FlightStatusResource']['Flights']['Flight'][0]['Arrival']['Terminal']['Gate']
+    except:
+        arrgate = ""
+    try:
+        arrtimestatus = data['FlightStatusResource']['Flights']['Flight'][0]['Arrival']['TimeStatus']['Code']
+    except:
+        arrtimestatus = ""
+    try:
+        arrtimestatusdef = data['FlightStatusResource']['Flights']['Flight'][0]['Arrival']['TimeStatus']['Definition']
+    except:
+        arrtimestatusdef = ""
+    try:
+        aircraftcode = data['FlightStatusResource']['Flights']['Flight'][0]['Equipment']['AircraftCode']
+    except:
+        aircraftcode = ""
+    try:
+        aircraftreg = data['FlightStatusResource']['Flights']['Flight'][0]['Equipment']['AircraftRegistration']
+    except:
+        aircraftreg = ""
+    try:
+        airlineid = data['FlightStatusResource']['Flights']['Flight'][0]['OperatingCarrier']['AirlineID']
+    except:
+        airlineid = ""
+    try:
+        flightnumber = data['FlightStatusResource']['Flights']['Flight'][0]['OperatingCarrier']['FlightNumber']
+    except:
+        flightnumber = ""
+    try:
+        flightstatus = data['FlightStatusResource']['Flights']['Flight'][0]['FlightStatus']['Code']
+    except:
+        flightstatus = ""
+    try:
+        flightstatusdef = data['FlightStatusResource']['Flights']['Flight'][0]['FlightStatus']['Definition']
+    except:
+        flightstatusdef = ""
+
+    newstatus = flight_status(depairport,depscheduled,depscheduledUTC,depactual,depactualUTC,depterminal,depgate,deptimestatus,deptimestatusdef,arrairport,arrscheduled,arrscheduledUTC,arractual,arractualUTC,arrterminal,arrgate,arrtimestatus,arrtimestatusdef,aircraftcode,aircraftreg,airlineid,flightnumber,flightstatus,flightstatusdef)
+    return newstatus
+
+
+token = "null"
+expires_date = "2000-01-01 00:00:00.0"
+def getToken():
+    global expires_date
+    global token
+    expires = datetime.datetime.strptime(str(expires_date), '%Y-%m-%d %H:%M:%S.%f')
+    if (expires < datetime.datetime.now()):
+        token, expires_date = getNewToken()
+    return token
+
+def getNewToken():
+    url = 'https://api.lufthansa.com/v1/oauth/token'
+    consumer = oauth.Consumer(key ='', secret='')
+    client = oauth.Client(consumer)
+    id, secret = readLHAccount()
+    params = "client_id=" + id + "&" + "client_secret=" + secret + "&grant_type=client_credentials"
+    print(params)
+    i = 0
+    while i < 10:
+        try:
+            resp, content = client.request(
+                            url,
+                            method = "POST",
+                            body=params,
+                            headers={'Content-type': 'application/x-www-form-urlencoded'}
+                            #force_auth_header=True
+                            )
+            content_string = content.decode("utf-8")
+            data = json.loads(content_string)
+            access_token = data["access_token"]
+            expires_in = data["expires_in"]
+            expires_date = datetime.datetime.now() + datetime.timedelta(0, (expires_in - 15))
+            print("New token, expires: ", expires_date)
+            i=10
+        except:
+            time.sleep(10)
+            if (i>3):
+                time.sleep(180)
+            if (i>5):
+                time.sleep(600)
+            print("Retry LH token " + str(i))
+            i = i + 1
+            pass
+
+    return access_token, expires_date
+
+
+def readLHAccount():
+    import os
+    path = os.path.abspath(os.path.dirname(__file__))
+    fullpath = os.path.join(path, "authentication/LHaccount.txt")
+    f = open(fullpath, "r")
+    lines = f.read().splitlines()
+    id = lines[0]
+    secret = lines[1]
+    f.close()
+    return id,secret
+
+
+class flight_status:
+    depairport = ""
+    depscheduled = ""
+    depscheduledUTC = ""
+    depactual = ""
+    depactualUTC = ""
+    depterminal = ""
+    depgate = ""
+    deptimestatus = ""
+    deptimestatusdef = ""
+    arrairport = ""
+    arrscheduled = ""
+    arrscheduledUTC = ""
+    arractual = ""
+    arractualUTC = ""
+    arrterminal = ""
+    arrgate = ""
+    arrtimestatus = ""
+    arrtimestatusdef = ""
+    aircraftcode = ""
+    aircraftreg = ""
+    airlineid = ""
+    flightnumber = ""
+    flightstatus = ""
+    flightstatusdef = ""
+
+    def __init__(self, depairport,depscheduled,depscheduledUTC,depactual,depactualUTC,depterminal,depgate,deptimestatus,deptimestatusdef,arrairport,arrscheduled,arrscheduledUTC,arractual,arractualUTC,arrterminal,arrgate,arrtimestatus,arrtimestatusdef,aircraftcode,aircraftreg,airlineid,flightnumber,flightstatus,flightstatusdef):
+        self.depairport = depairport
+        self.depscheduled = depscheduled
+        self.depscheduledUTC = depscheduledUTC
+        self.depactual = depactual
+        self.depactualUTC = depactualUTC
+        self.depterminal = depterminal
+        self.depgate = depgate
+        self.deptimestatus = deptimestatus
+        self.deptimestatusdef = deptimestatusdef
+        self.arrairport = arrairport
+        self.arrscheduled = arrscheduled
+        self.arrscheduledUTC = arrscheduledUTC
+        self.arractual = arractual
+        self.arractualUTC = arractualUTC
+        self.arrterminal = arrterminal
+        self.arrgate = arrgate
+        self.arrtimestatus = arrtimestatus
+        self.arrtimestatusdef = arrtimestatusdef
+        self.aircraftcode = aircraftcode
+        self.aircraftreg = aircraftreg
+        self.airlineid = airlineid
+        self.flightnumber = flightnumber
+        self.flightstatus = flightstatus
+        self.flightstatusdef = flightstatusdef
---
+++ telegrambot/main.py
@@ -0,0 +1,156 @@
+import logging
+from getflight import getFlightStatus
+from telegram import (ReplyKeyboardMarkup, ReplyKeyboardRemove)
+from telegram.ext import (Updater, CommandHandler, MessageHandler, Filters,
+                          ConversationHandler)
+
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO, filename="newfile.log", filemode='w')
+
+logger = logging.getLogger(__name__)
+
+START, FLIGHT, FLIGHTDATE, STATUSMORE = range(4)
+
+def start(update, context):
+    reply_keyboard = [['OK']]
+    update.message.reply_text(
+        'Hello, I can check flight status :)', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return START
+
+
+def flight(update, context):
+    reply_keyboard = [['LH3526', 'LH122', 'EN8860']]
+    update.message.reply_text(
+        'Flight Number (LH000):', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return FLIGHT
+
+
+def flightdate(update, context):
+    reply_keyboard = [['2020-03-30', '2020-03-31', '2020-04-01']]
+    user = update.message.from_user
+    logger.info("FLight of %s: %s", user.first_name, update.message.text)
+    context.user_data['flight'] = update.message.text
+    update.message.reply_text('Flight Date (YYYY-MM-DD):', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return FLIGHTDATE
+
+
+def flightstatus(update, context):
+    reply_keyboard = [['Departure', 'Arrival', 'Equipment']]
+    user = update.message.from_user
+    logger.info("Date of %s: %s", user.first_name, update.message.text)
+    context.user_data['flightdate'] = update.message.text
+    flightstatus = getFlightStatus(context.user_data["flight"], context.user_data["flightdate"])
+    context.user_data['flightstatus'] = flightstatus
+    if (flightstatus.depairport != ""):
+        update.message.reply_text('{} {}\n'
+                                  '{} -> {}\n'
+                                  'Status: {}'.format(context.user_data["flight"],context.user_data["flightdate"],flightstatus.depairport,flightstatus.arrairport,flightstatus.flightstatusdef), reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+    else:
+        reply_keyboard = [['LH3526', 'LH122', 'EN8860']]
+        update.message.reply_text('This flight does not exist, try another!', reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+        return FLIGHT
+
+    return STATUSMORE
+
+def departure(update, context):
+    reply_keyboard = [['Done', 'Arrival', 'Equipment']]
+    user = update.message.from_user
+    logger.info("Option of %s: %s", user.first_name, update.message.text)
+    flightstatus = context.user_data['flightstatus']
+    update.message.reply_text('Scheduled: {}\n'
+                                  'Actual: {}\n'
+                                  'Time Status: {}\n'
+                                  'Terminal: {}\n'
+                                  'Gate: {}'.format(flightstatus.depscheduled,flightstatus.depactual,flightstatus.deptimestatusdef,flightstatus.depterminal,flightstatus.depgate), reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return STATUSMORE
+
+def arrival(update, context):
+    reply_keyboard = [['Departure', 'Done', 'Equipment']]
+    user = update.message.from_user
+    logger.info("Option of %s: %s", user.first_name, update.message.text)
+    flightstatus = context.user_data['flightstatus']
+    update.message.reply_text('Scheduled: {}\n'
+                                  'Actual: {}\n'
+                                  'Time Status: {}\n'
+                                  'Terminal: {}\n'
+                                  'Gate: {}'.format(flightstatus.arrscheduled,flightstatus.arractual,flightstatus.arrtimestatusdef,flightstatus.arrterminal,flightstatus.arrgate), reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return STATUSMORE
+
+def equipment(update, context):
+    reply_keyboard = [['Departure', 'Arrival', 'Done']]
+    user = update.message.from_user
+    logger.info("Option of %s: %s", user.first_name, update.message.text)
+    flightstatus = context.user_data['flightstatus']
+    update.message.reply_text('Operating Carrier Flight: {}{}\n'
+                                  'Aircraft: {}\n'
+                                  'Aircraft Registration\n'.format(flightstatus.airlineid,flightstatus.flightnumber,flightstatus.aircraftcode,flightstatus.aircraftreg), reply_markup=ReplyKeyboardMarkup(reply_keyboard, one_time_keyboard=True))
+
+    return STATUSMORE
+
+def cancel(update, context):
+    user = update.message.from_user
+    logger.info("User %s canceled the conversation.", user.first_name)
+    update.message.reply_text('Bye',
+                              reply_markup=ReplyKeyboardRemove())
+    user_data.clear()
+
+    return ConversationHandler.END
+
+
+def error(update, context):
+    """Log Errors caused by Updates."""
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+
+def main():
+    bot_token = readTGAccount()
+    updater = Updater(bot_token, use_context=True)
+
+    dp = updater.dispatcher
+
+    conv_handler = ConversationHandler(
+        entry_points=[CommandHandler('start', start)],
+
+        states={
+
+            START: [MessageHandler(Filters.text, flight)],
+
+            FLIGHT: [MessageHandler(Filters.regex('^[A-z][A-z][0-9]{1,4}$'), flightdate)],
+
+            FLIGHTDATE: [MessageHandler(Filters.regex('^([12]\d{3}-(0[1-9]|1[0-2])-(0[1-9]|[12]\d|3[01]))$'), flightstatus)],
+
+            STATUSMORE: [MessageHandler(Filters.regex('^Departure$'), departure),
+                         MessageHandler(Filters.regex('^Arrival$'), arrival),
+                         MessageHandler(Filters.regex('^Equipment$'), equipment),
+                         MessageHandler(Filters.regex('^Done$'), flight)]
+        },
+
+        fallbacks=[CommandHandler('cancel', cancel),CommandHandler('start', start)]
+    )
+
+    dp.add_handler(conv_handler)
+
+    dp.add_error_handler(error)
+
+    updater.start_polling()
+
+    updater.idle()
+
+def readTGAccount():
+    import os
+    path = os.path.abspath(os.path.dirname(__file__))
+    fullpath = os.path.join(path, "authentication/TGaccount.txt")
+    f = open(fullpath, "r")
+    lines = f.read().splitlines()
+    bot_token = lines[0]
+    f.close()
+    return bot_token
+
+if __name__ == '__main__':
+    main()
