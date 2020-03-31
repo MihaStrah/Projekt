58593e5b3e5b664e287834700374f9a2cca588fb
mihastrah <miha.strah@student.um.si>
2020-03-31 13:12:50 +0200

    changed schedule

--- lhgetdata/main.py
+++ lhgetdata/main.py
@@ -7,24 +7,21 @@ import datetime
 
 def job():
     print("DATETIME: ", datetime.datetime.now(), " checking hour")
-    if (datetime.datetime.now().strftime("%H") == "00"):
-        print("DATETIME: ", datetime.datetime.now(), " hour is 00")
-        #dateToday = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
-        dateToday = (datetime.date.today() - datetime.timedelta(days=2)).strftime("%Y-%m-%d")
-        print("DATETIME: ", datetime.datetime.now(), " ::: Job in progress ... getting flights for date: ", dateToday)
+    if (datetime.datetime.now().strftime("%H") == "21"):
+        print("DATETIME: ", datetime.datetime.now(), " hour is 21")
+        dateFlight = (datetime.date.today() - datetime.timedelta(days=1)).strftime("%Y-%m-%d")
+        print("DATETIME: ", datetime.datetime.now(), " ::: Job in progress ... getting flights for date: ", dateFlight)
         token = getNewToken()
         allflights, allids = getAllFLights()
-        getFlightStatusWriteSql(token,allflights,allids,dateToday,3)
-        print("DATETIME: ", datetime.datetime.now(), "End of job, all flights processed for date: ", dateToday)
+        getFlightStatusWriteSql(token,allflights,allids,dateFlight,3)
+        print("DATETIME: ", datetime.datetime.now(), "End of job, all flights processed for date: ", dateFlight)
     print("DATETIME: ", datetime.datetime.now(), "hour is not 00")
     return
 
-#schedule.every().day.at("00:30").do(job)
-#spremeni datum nazaj na dan prej ko spremeniš uro!
+#schedule.every().day.at("21:01").do(job) NE DELA
 
-#testno
-schedule.every().hour.at(":45").do(job)
-
+#workaround
+schedule.every().hour.at(":01").do(job)
 
 
 while True:
