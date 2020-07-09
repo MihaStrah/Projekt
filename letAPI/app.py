ac7779d7e30cd02c2ae51fabededf78d7e245e8b
mihastrah <miha.strah@student.um.si>
2020-07-09 15:24:38 +0200

    added letAPI

---
+++ letAPI/app.py
@@ -0,0 +1,134 @@
+from flask import Flask, request, jsonify, make_response
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import uuid
+import jwt
+import datetime
+from functools import wraps
+import os
+import logging
+from izbaze import getSQLFlightStatus, getSQLFlightStats
+
+app = Flask(__name__)
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+                    level=logging.INFO, filename="letAPI_logs_out/letAPIPythonScriptLog.log", filemode='a')
+logger = logging.getLogger(__name__)
+
+def readAPIKey():
+    import os
+    path = os.path.abspath(os.path.dirname(__file__))
+    fullpath = os.path.join(path, "authentication/APIKey.txt")
+    f = open(fullpath, "r")
+    lines = f.read().splitlines()
+    key = lines[0]
+    f.close()
+    return key
+
+
+path = os.path.abspath(os.path.dirname(__file__))
+fullpath = 'sqlite:///' + os.path.join(path, 'apiusers/APIusersDB.db')
+print(fullpath)
+
+app.config['SECRET_KEY'] = readAPIKey()
+app.config['SQLALCHEMY_DATABASE_URI'] = (fullpath)
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+
+db = SQLAlchemy(app)
+
+
+class Users(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    public_id = db.Column(db.Integer)
+    name = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    admin = db.Column(db.Boolean)
+
+
+class Authors(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), unique=True, nullable=False)
+    book = db.Column(db.String(20), unique=True, nullable=False)
+    country = db.Column(db.String(50), nullable=False)
+    booker_prize = db.Column(db.Boolean)
+    user_id = db.Column(db.Integer)
+
+db.create_all()
+print('db created')
+
+
+def token_required(f):
+    @wraps(f)
+    def decorator(*args, **kwargs):
+
+        token = None
+
+        if 'x-access-tokens' in request.headers:
+            token = request.headers['x-access-tokens']
+
+        if not token:
+            return jsonify({'message': 'a valid token is missing'})
+
+        try:
+            data = jwt.decode(token, app.config['SECRET_KEY'])
+            current_user = Users.query.filter_by(public_id=data['public_id']).first()
+        except:
+            return jsonify({'message': 'token is invalid'})
+
+        return f(current_user, *args, **kwargs)
+
+    return decorator
+
+
+@app.route('/login', methods=['POST'])
+def login_user():
+    auth = request.authorization
+
+    if not auth or not auth.username or not auth.password:
+        return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
+
+    user = Users.query.filter_by(name=auth.username).first()
+
+    if check_password_hash(user.password, auth.password):
+        exp = datetime.datetime.utcnow() + datetime.timedelta(minutes=30)
+        token = jwt.encode(
+            {'public_id': user.public_id, 'exp': exp},
+            app.config['SECRET_KEY'])
+        return jsonify({'token': token.decode('UTF-8'), 'expires': exp})
+
+    return make_response('could not verify', 401, {'WWW.Authentication': 'Basic realm: "login required"'})
+
+
+#let
+@app.route('/let/<datum>/<stlet>', methods=['GET'])
+@token_required
+def get_let(current_user,datum,stlet):
+    print(datum)
+    print(stlet)
+    letinfo = getSQLFlightStatus(stlet,datum)
+    print(letinfo)
+    return (letinfo)
+
+#letstat7day
+@app.route('/stat7/<stlet>', methods=['GET'])
+@token_required
+def get_letstat7day(current_user,stlet):
+    print(stlet)
+    letstat7dayinfo = getSQLFlightStats(stlet,7)
+    print(letstat7dayinfo)
+    return (letstat7dayinfo)
+
+#letstat7day
+@app.route('/stat30/<stlet>', methods=['GET'])
+@token_required
+def get_letstat30day(current_user,stlet):
+    print(stlet)
+    letstat30dayinfo = getSQLFlightStats(stlet,30)
+    print(letstat30dayinfo)
+    return (letstat30dayinfo)
+
+
+#if __name__ == '__main__':
+#    app.run(debug=True)
+
+
---
+++ letAPI/izbaze.py
@@ -0,0 +1,234 @@
+import time
+import mysql.connector as mariadb
+import re
+import logging
+import json
+from flask import jsonify
+
+logger = logging.getLogger(__name__)
+
+def getSQLFlightStatus(flight,date):
+    host, port, database, user, password = readDBAccount()
+
+    airlineid = re.search("[A-z]{2}", flight).group()
+    flightnumber = re.search("[0-9]{1,4}", flight).group()
+    #padding to min length 3
+    flightnumber = str(flightnumber).zfill(3)
+
+    i=0
+    while i < 10:
+        try:
+            mariadb_connection = mariadb.connect(user=user, password=password, database=database, host=host, port=port)
+            cursor = mariadb_connection.cursor()
+            i = 10
+            logger.info("DB connection successful")
+        except:
+            i = i + 1
+            time.sleep(1)
+            if (i > 3):
+                time.sleep(2)
+            if (i > 5):
+                time.sleep(3)
+            #print("Retry DB " + str(i))
+            if (i == 10):
+                logger.error("DB error, ABORT")
+            else:
+                logger.info("DB error, retry")
+            pass
+
+    i = 0
+    while i < 3:
+        try:
+            #search in DB for operating and for codeshare (returns operating flight)
+            cursor.execute("SELECT depairport,depscheduled,depscheduledUTC,depactual,depactualUTC,depterminal,depgate,deptimestatus,arrairport,arrscheduled,arrscheduledUTC,arractual,arractualUTC,arrterminal,arrgate,arrtimestatus,aircraftcode,aircraftreg,airlineid,flightnumber,flightstatus,flightnumberkey from allflightsstatus where id in (select operating_id from codeshares where DATE(depscheduled)=DATE(%(date)s) and ((codeshare_airlineid=%(airlineid)s and codeshare_flightnumber=%(flightnumber)s) OR (operating_airlineid=%(airlineid)s and operating_flightnumber=%(flightnumber)s )) order by depscheduled DESC ) or DATE(depscheduled)=DATE(%(date)s) and airlineid=%(airlineid)s and flightnumber=%(flightnumber)s order by depscheduled DESC limit 1;",{'airlineid': str(airlineid), 'flightnumber': str(flightnumber), 'date': str(date)})
+            #search in DB only for operating flights
+            #cursor.execute("SELECT depairport,depscheduled,depscheduledUTC,depactual,depactualUTC,depterminal,depgate,deptimestatus,arrairport,arrscheduled,arrscheduledUTC,arractual,arractualUTC,arrterminal,arrgate,arrtimestatus,aircraftcode,aircraftreg,airlineid,flightnumber,flightstatus,flightnumberkey from allflightsstatus where airlineid=%(airlineid)s and flightnumber=%(flightnumber)s and DATE(depscheduled)=DATE(%(date)s) limit 1;", {'airlineid': str(airlineid), 'flightnumber': str(flightnumber),'date': str(date)})
+            mariadb_connection.close()
+            i = 3
+        except mariadb.Error as error:
+            i = i + 1
+            time.sleep(2)
+            #print("Mariadb Error: {}".format(error))
+            #print("Retry DB SELECT " + str(i))
+            logger.error("DB error: %s", error)
+            if (i==3):
+                logger.error("DB error, ABORT")
+            else:
+                logger.info("DB error, retry")
+            pass
+
+        returnrow = cursor.fetchone()
+        if returnrow is None:
+            newstatus = jsonify({'info': 'let ne obstaja'})
+        else:
+            newstatus = (flight_status(returnrow[0],returnrow[1],returnrow[2],returnrow[3],returnrow[4],returnrow[5],returnrow[6],returnrow[7],returnrow[8],returnrow[9],returnrow[10],returnrow[11],returnrow[12],returnrow[13],returnrow[14],returnrow[15],returnrow[16],returnrow[17],returnrow[18],returnrow[19],returnrow[20])).toJson()
+
+    return newstatus
+
+
+def getSQLFlightStats(flight,days):
+    host, port, database, user, password = readDBAccount()
+    airlineid = re.search("[A-z]{1,2}", flight).group()
+    flightnumber = re.search("[0-9]{1,5}", flight).group()
+    #padding to min length 3
+    flightnumber = str(flightnumber).zfill(3)
+
+    i=0
+    while i < 10:
+        try:
+            mariadb_connection = mariadb.connect(user=user, password=password, database=database, host=host, port=port)
+            cursor = mariadb_connection.cursor()
+            i = 10
+            logger.info("DB connection successful")
+        except:
+            i = i + 1
+            time.sleep(1)
+            if (i > 3):
+                time.sleep(2)
+            if (i > 5):
+                time.sleep(3)
+            #print("Retry DB " + str(i))
+            if (i == 10):
+                logger.error("DB error, ABORT")
+            else:
+                logger.info("DB error, retry")
+            pass
+
+    i = 0
+    while i < 3:
+        try:
+            #search in DB for operating and for codeshare (returns operating flight)
+            if (days == 7):
+                cursor.execute("select Allflights, Cancelled, Dep_OT, Dep_FE, Dep_DL, AverageTimeDep, AverageTimeDep_OT, AverageTimeDep_FE, AverageTimeDep_DL, Arr_OT, Arr_FE, Arr_DL, AverageTimeArr, AverageTimeArr_OT, AverageTimeArr_FE, AverageTimeArr_DL from flightstats_7day where allflights_id in (select max(flightnumberkey) from allflightsstatus where (id in (select max(operating_id) from codeshares where codeshare_airlineid=%(airlineid)s and codeshare_flightnumber=%(flightnumber)s)) or (airlineid=%(airlineid)s and flightnumber=%(flightnumber)s));",{'airlineid': str(airlineid), 'flightnumber': str(flightnumber)})
+            else:
+                cursor.execute("select Allflights, Cancelled, Dep_OT, Dep_FE, Dep_DL, AverageTimeDep, AverageTimeDep_OT, AverageTimeDep_FE, AverageTimeDep_DL, Arr_OT, Arr_FE, Arr_DL, AverageTimeArr, AverageTimeArr_OT, AverageTimeArr_FE, AverageTimeArr_DL from flightstats_30day where allflights_id in (select max(flightnumberkey) from allflightsstatus where (id in (select max(operating_id) from codeshares where codeshare_airlineid=%(airlineid)s and codeshare_flightnumber=%(flightnumber)s)) or (airlineid=%(airlineid)s and flightnumber=%(flightnumber)s));",{'airlineid': str(airlineid), 'flightnumber': str(flightnumber)})
+
+            #search in DB only for operating flights
+            #cursor.execute("SELECT depairport,depscheduled,depscheduledUTC,depactual,depactualUTC,depterminal,depgate,deptimestatus,arrairport,arrscheduled,arrscheduledUTC,arractual,arractualUTC,arrterminal,arrgate,arrtimestatus,aircraftcode,aircraftreg,airlineid,flightnumber,flightstatus,flightnumberkey from allflightsstatus where airlineid=%(airlineid)s and flightnumber=%(flightnumber)s and DATE(depscheduled)=DATE(%(date)s) limit 1;", {'airlineid': str(airlineid), 'flightnumber': str(flightnumber),'date': str(date)})
+            mariadb_connection.close()
+            i = 3
+        except mariadb.Error as error:
+            i = i + 1
+            time.sleep(2)
+            #print("Mariadb Error: {}".format(error))
+            #print("Retry DB SELECT " + str(i))
+            logger.error("DB error: %s", error)
+            if (i==3):
+                logger.error("DB error, ABORT")
+            else:
+                logger.info("DB error, retry")
+            pass
+
+        returnrow = cursor.fetchone()
+
+        print(returnrow)
+
+        if returnrow is None:
+            letstats = jsonify({'info': 'statistika za ta let ne obstaja'})
+        else:
+            letstats = (flight_stats(returnrow[0],returnrow[1],returnrow[2],returnrow[3],returnrow[4],returnrow[5],returnrow[6],returnrow[7],returnrow[8],returnrow[9],returnrow[10],returnrow[11],returnrow[12],returnrow[13],returnrow[14],returnrow[15])).toJson()
+
+    return letstats
+
+def readDBAccount():
+    import os
+    path = os.path.abspath(os.path.dirname(__file__))
+    fullpath = os.path.join(path, "authentication/DBaccount.txt")
+    f = open(fullpath, "r")
+    lines = f.read().splitlines()
+    host = lines[0]
+    port = lines[1]
+    database = lines[2]
+    user = lines[3]
+    password = lines[4]
+    f.close()
+    return host, port, database, user, password
+
+class flight_status:
+    depairport = ""
+    depscheduled = ""
+    depscheduledUTC = ""
+    depactual = ""
+    depactualUTC = ""
+    depterminal = ""
+    depgate = ""
+    deptimestatus = ""
+    arrairport = ""
+    arrscheduled = ""
+    arrscheduledUTC = ""
+    arractual = ""
+    arractualUTC = ""
+    arrterminal = ""
+    arrgate = ""
+    arrtimestatus = ""
+    aircraftcode = ""
+    aircraftreg = ""
+    airlineid = ""
+    flightnumber = ""
+    flightstatus = ""
+
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
+
+    def __init__(self, depairport,depscheduled,depscheduledUTC,depactual,depactualUTC,depterminal,depgate,deptimestatus,arrairport,arrscheduled,arrscheduledUTC,arractual,arractualUTC,arrterminal,arrgate,arrtimestatus,aircraftcode,aircraftreg,airlineid,flightnumber,flightstatus):
+        self.depairport = depairport
+        self.depscheduled = depscheduled
+        self.depscheduledUTC = depscheduledUTC
+        self.depactual = depactual
+        self.depactualUTC = depactualUTC
+        self.depterminal = depterminal
+        self.depgate = depgate
+        self.deptimestatus = deptimestatus
+        self.arrairport = arrairport
+        self.arrscheduled = arrscheduled
+        self.arrscheduledUTC = arrscheduledUTC
+        self.arractual = arractual
+        self.arractualUTC = arractualUTC
+        self.arrterminal = arrterminal
+        self.arrgate = arrgate
+        self.arrtimestatus = arrtimestatus
+        self.aircraftcode = aircraftcode
+        self.aircraftreg = aircraftreg
+        self.airlineid = airlineid
+        self.flightnumber = flightnumber
+        self.flightstatus = flightstatus
+
+
+class flight_stats:
+    Allflights = ""
+    Cancelled = ""
+    Dep_OT = ""
+    Dep_FE = ""
+    Dep_DL = ""
+    AverageTimeDep = ""
+    AverageTimeDep_OT = ""
+    AverageTimeDep_FE = ""
+    AverageTimeDep_DL = ""
+    Arr_OT = ""
+    Arr_FE = ""
+    Arr_DL = ""
+    AverageTimeArr = ""
+    AverageTimeArr_OT = ""
+    AverageTimeArr_FE = ""
+    AverageTimeArr_DL = ""
+
+    def toJson(self):
+        return json.dumps(self, default=lambda o: o.__dict__)
+
+    def __init__(self, Allflights, Cancelled, Dep_OT, Dep_FE, Dep_DL, AverageTimeDep, AverageTimeDep_OT, AverageTimeDep_FE, AverageTimeDep_DL, Arr_OT, Arr_FE, Arr_DL, AverageTimeArr, AverageTimeArr_OT, AverageTimeArr_FE, AverageTimeArr_DL):
+        self.Allflights = Allflights
+        self.Cancelled = Cancelled
+        self.Dep_OT = Dep_OT
+        self.Dep_FE = Dep_FE
+        self.Dep_DL = Dep_DL
+        self.AverageTimeDep = AverageTimeDep
+        self.AverageTimeDep_OT = AverageTimeDep_OT
+        self.AverageTimeDep_FE = AverageTimeDep_FE
+        self.AverageTimeDep_DL = AverageTimeDep_DL
+        self.Arr_OT = Arr_OT
+        self.Arr_FE = Arr_FE
+        self.Arr_DL = Arr_DL
+        self.AverageTimeArr = AverageTimeArr
+        self.AverageTimeArr_OT = AverageTimeArr_OT
+        self.AverageTimeArr_FE = AverageTimeArr_FE
+        self.AverageTimeArr_DL = AverageTimeArr_DL
