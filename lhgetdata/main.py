1c0b96d84baa7814397c47ccf5a3266613f10457
mihastrah <miha.strah@student.um.si>
2020-07-24 23:01:38 +0200

    new api ver

--- lhgetdata/main.py
+++ lhgetdata/main.py
@@ -18,7 +18,7 @@ def job():
     # workaround
 
     #change to 18 (this timezone 20:30)
-    if (datetime.datetime.now().strftime("%H") == "20"):
+    if (datetime.datetime.now().strftime("%H") == "21"):
         #print("DATETIME: ", datetime.datetime.now(), " Hour is 20")
 
 
@@ -71,7 +71,7 @@ def job():
 logger.info("lhgetdata started")
 print("lghetdata started, logging in lhgetdataPythonScriptLog.log")
 #change to 30
-schedule.every().hour.at(":45").do(job)
+schedule.every().hour.at(":05").do(job)
 
 while True:
     schedule.run_pending()
