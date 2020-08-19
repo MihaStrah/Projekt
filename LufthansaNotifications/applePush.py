973e177120ab85b316d5a3d442130a007e386882
mihastrah <miha.strah@student.um.si>
2020-08-19 17:58:14 +0200

    new api and notifications

--- LufthansaNotifications/applePush.py
+++ LufthansaNotifications/applePush.py
@@ -17,7 +17,7 @@ def sendMultipleNotifications(tokens, title, body, flightString, dateString):
     topic = 'com.MihaStrah.FlightTracker'
     #client = APNsClient('AppleAuthentication/pushcertdev.pem', use_sandbox=True, use_alternative_port=False)
     token_credentials = TokenCredentials(auth_key_path="AppleAuthentication/AuthKey_85KZTANBJ8.p8", auth_key_id="85KZTANBJ8", team_id="7YNLV7443U")
-    client = APNsClient(credentials=token_credentials, use_sandbox=True)
+    client = APNsClient(credentials=token_credentials, use_sandbox=False)
     Notification = collections.namedtuple('Notification', ['token', 'payload'])
     notifications = []
 
